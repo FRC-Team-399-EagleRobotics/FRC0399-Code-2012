0c7422ca93b2cad1a5b5356e07d6c448dd557d39
Jeremy P Germita <jeremy.germita@gmail.com>
2012-05-30 00:46:32 -0700

    Some AutoShooterSpeedController stuff
    changing the algorithm a bit

--- X-1_JPGermita/src/org/team399/y2012/robot/Controls/Automation/AutoShooterSpeedController.java
+++ X-1_JPGermita/src/org/team399/y2012/robot/Controls/Automation/AutoShooterSpeedController.java
@@ -12,33 +12,47 @@ import org.team399.y2012.robot.Systems.Shooter;
  * @author Jeremy
  */
 public class AutoShooterSpeedController {
+
     private Shooter m_shooter;
+    private double[][] setDistRanges_LUT = {
+        {0, 0, 0}, //Fender front shot distance range
+        {0, 0, 0} //Key Shot distance range
+    };
 
     /**
      * Constructor
      */
     public AutoShooterSpeedController() {
     }
-    
+
     public double distanceToRPM(double distance) {
 //        return distance;
-        
+
         /*double answer = MathUtils.pow(distance-3000, 3);
         answer *= .000000003;
         answer += 60;
         return answer;*/
-        
+
+
         double midRange = 60;
         double maxRange = 120;
         double midRPM = 3000;
         double maxRPM = 4100;
         double scale = .000000003;
-        
-        double answer = MathUtils.pow((distance - midRange)*scale, 1/3)+midRPM;
-        answer = (answer > maxRPM) ? maxRPM : answer;
-        answer = (answer < 0) ? 0 : answer;
-        
+        double answer = 0;
+
+        if (distance >= setDistRanges_LUT[0][0] && distance <= setDistRanges_LUT[0][1]) {
+            //Fender Shot
+            answer = setDistRanges_LUT[0][2];
+        } else if (distance >= setDistRanges_LUT[1][0] && distance <= setDistRanges_LUT[1][1]) {
+            //Key Shot
+            answer = setDistRanges_LUT[1][2];
+        } else {
+            answer = Math.sqrt((distance - midRange) * scale) + midRPM;
+            answer = (answer > maxRPM) ? maxRPM : answer;
+            answer = (answer < 0) ? 0 : answer;
+        }
+
         return answer;
     }
-    
 }
